fbc86595d286930179fb6180cf92741cceaafe41
Oleg Kalachev <okalachev@gmail.com>
2021-10-26 07:48:34 +0300

    Add managing app

--- README.md
+++ README.md
@@ -18,3 +18,36 @@ Access the drone using web tools:
 * `<ip_address>:8080` – Clover's web tools. Use external camera view to monitor your simulation.
 * `<ip_address>:57575` – Butterfly terminal.
 * `<ip_address>:7070` – gzweb.
+
+## Running managed simulation playground
+
+Prerequisites: Ubuntu Linux (20.04+ is recommended), git, Python 3, pip.
+
+Clone the repo:
+
+```bash
+git clone https://github.com/CopterExpress/clover_cloud.git
+cd clover_cloud
+```
+
+Install Python dependencies (for `root` user):
+
+```bash
+sudo pip3 install -r requirements.txt
+```
+
+Make an environment file for systemd service:
+
+```bash
+echo "CLOVER_CLOUD_DIR=$(pwd)" | sudo tee -a /etc/systemd/system/clover-cloud-app.env
+```
+
+Install and run systemd services:
+
+```bash
+sudo ln -s $(realpath *.service) /lib/systemd/system
+sudo systemctl enable clover-cloud-app
+sudo systemctl start clover-cloud-app
+```
+
+Access managing app using `http://<ip_address>`.
---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, send_from_directory
+from flask_limiter import Limiter
+import subprocess
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return send_from_directory('www', 'index.html')
+
+
+@app.route('/frames')
+def frames():
+    return send_from_directory('www', 'frames.html')
+
+
+def run(cmd):
+    result = subprocess.run(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode == os.EX_OK:
+        print('stdout', repr(result.stdout))
+        return result.stdout or '<meta http-equiv="refresh" content="1;url=/">\nok', 200
+    else:
+        return result.stderr.decode('utf-8') or 'error', 500
+
+
+limiter = Limiter(app, key_func=lambda: 1) # limiter for all users, NOTE: only for one worker
+
+
+@app.route('/reset', methods=['POST'])
+@limiter.limit("1 per 10 seconds")
+def reset():
+    return run('sudo systemctl restart clover-cloud')
+
+
+@app.route('/stop', methods=['POST'])
+@limiter.limit("1 per 10 seconds")
+def stop():
+    return run('sudo systemctl stop clover-cloud')
+
+
+@app.route('/restart', methods=['POST'])
+def restart():
+    return 'not implemented', 501
+
+
+@app.route('/status')
+def status():
+    result = subprocess.run('systemctl is-active clover-cloud'.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.stderr or result.stdout
+
+
+@app.route('/log')
+def log():
+    return subprocess.check_output('sudo journalctl _SYSTEMD_INVOCATION_ID=$(systemctl show -p InvocationID --value clover-cloud.service) -o cat --no-pager', shell=True)
